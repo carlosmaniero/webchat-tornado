aea8d094c843a8f04f97c0b2001c41a63e573643
Carlos Maniero <carlosmaniero@gmail.com>
2014-12-04 21:06:25 -0200

    Fix handler

--- users/handlers.py
+++ users/handlers.py
@@ -5,6 +5,7 @@ from tornado.options import options
 from tornado.web import HTTPError
 from core.handlers import ServerBaseHandler
 from users.buffer import global_user_buffer
+import json
 
 
 class UserHandler(ServerBaseHandler):
@@ -22,6 +23,17 @@ class UserHandler(ServerBaseHandler):
             raise HTTPError(405)
 
     def post(self, *args, **kwargs):
-        data = json_decode(self.request.body)
+        data = {
+            'id': self.get_argument('id'),
+            'first_name': self.get_argument('first_name'),
+            'last_name': self.get_argument('last_name'),
+            'nickname': self.get_argument('nickname'),
+            'email': self.get_argument('email'),
+            'thumb': self.get_argument('thumb')
+        }
+
+        if data['id'] is None or data['email'] is None:
+            raise HTTPError(400, 'User need an id and a email')
+
         token = global_user_buffer.add_user(data)
         self.write({'token': token})
